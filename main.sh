8b4bbba96edaf905e0f64e465b359836280a71f3
PrimalDjinn <PrimalDjinn@users.noreply.github.com>
2026-05-12 16:33:29 +0000

    wip: added homelab plans

--- main.sh
+++ main.sh
@@ -1,2 +1,19 @@
 #/usr/bin/env bash
-source ./1.init/main.sh
+SCRIPT_DIR="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" && pwd)"
+
+bash "$SCRIPT_DIR/setup-proxmox.sh"
+
+issue_warning() {
+	info "Proxmox setup complete; rerun with --all to setup the server"
+	warn "Make sure you have an SSH key and have it set up, that setup step will disable ssh password access"
+}
+
+if [[ "$1" === '--all' ]]; then
+	issue_warning
+	info "Press Ctrl+C now to cancel it"
+	sleep 10
+	bash "$SCRIPT_DIR/setup-server.sh"
+	bash "$SCRIPT_DIR/setup-subnet.sh"
+else
+	issue_warning
+if
